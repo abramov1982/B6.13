7d99ae974b65b32c56f309aa3c49127ef1e173c3
Ilya.Abramov <abramov1982@gmail.com>
2020-01-26 12:14:04 +0300

    Homework update

--- album.py
+++ album.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Описание подключения к базе данных
-DB_PATH = "sqlite:///albums.sqlite3"
+DB_PATH = 'sqlite:///albums.sqlite3'
 Base = declarative_base()
 
 # Устанавливает соединение к базе данных, создает таблицы, если их еще нет и возвращает объект сессии
@@ -17,7 +17,7 @@ session = connect_db()
 
 # Описание структуры таблиц в базе данных.
 class Album(Base):
-    __tablename__ = "album"
+    __tablename__ = 'album'
 
     id = sa.Column(sa.INTEGER, primary_key=True)
     year = sa.Column(sa.INTEGER)
@@ -25,13 +25,29 @@ class Album(Base):
     genre = sa.Column(sa.TEXT)
     album = sa.Column(sa.TEXT)
 
-#...
-
 def find(artist):
     albums = session.query(Album).filter(Album.artist == artist).all()
     if len(albums) == 0:
         return False
     return albums
 
-if not find("123123"):
-    print("123")
+def add(year, artist, genre, album):
+    assert isinstance(year, int), 'Incorrect date'
+    assert isinstance(artist, str), 'Incorrect artist'
+    assert isinstance(genre, str), 'Incorrect genre'
+    assert isinstance(album, str), 'Incorrect album'
+
+    session = connect_db()
+    album_add = session.query(Album).filter(Album.album == album, Album.artist == artist).first()
+    if album_add is not None:
+        raise AlreadyExists('Такой альбом уже есть и его имя: #{}'.format(saved_album.id))
+
+    album: Album = Album(
+        year=year,
+        artist=artist,
+        genre=genre,
+        album=album
+    )    
+    session.add(album)
+    session.commit()
+    return album
